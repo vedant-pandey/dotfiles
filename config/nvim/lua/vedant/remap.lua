c051d70440073d86f27594e14e0bc77c9906e6a2
Vedant Pandey <vedantpandey46@gmail.com>
2023-07-21 09:34:27 +0530

    More remaps

--- config/nvim/lua/vedant/remap.lua
+++ config/nvim/lua/vedant/remap.lua
@@ -8,6 +8,7 @@ vim.keymap.set("n", "<leader><CR>", ":so %<CR>")
 vim.keymap.set("n", "<leader>w", ":w<cr>")
 vim.keymap.set("n", "<leader>s", ":w<cr>:so %<CR>")
 vim.keymap.set("i", "jk", "<esc>")
+vim.keymap.set("n", "<BS>", "X")
 
 -- text editting skillz
 vim.keymap.set("n", "<leader>P", [["+P<CR>]])
@@ -27,7 +28,21 @@ vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
 
 -- window sizing
-vim.keymap.set("n", "<C-L>", "<C-w>>")
-vim.keymap.set("n", "<C-H>", "<C-w><")
-vim.keymap.set("n", "<C-J>", "<C-w>-")
-vim.keymap.set("n", "<C-K>", "<C-w>+")
+vim.keymap.set("n", "<M-L>", "<C-w>>")
+vim.keymap.set("n", "<M-H>", "<C-w><")
+vim.keymap.set("n", "<M-J>", "<C-w>-")
+vim.keymap.set("n", "<M-J>", "<C-w>-")
+vim.keymap.set("n", "<M-W>", "<C-w>=")
+
+-- Lunar vim remaps
+vim.keymap.set("v", "<leader>v;", "g<C-g>")
+vim.keymap.set("n", "<leader>vn", ":ene<CR>")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-b>", "<C-b>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- set after plugin installs
+-- vim.keymap.set("n", "<leader>x", ":BufferKill<CR>")
+-- vim.keymap.set("n", "<leader>vb", "<cmd>vsplit<CR><cmd>Telescope find_files<cr>" -- split buffer and select what to open)
+-- vim.keymap.set("n", "<leader>`", "<cmd>ToggleTerm<cr>")
--- config/nvim/lua/vedant/set.lua
+++ config/nvim/lua/vedant/set.lua
@@ -7,3 +7,4 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.wrap = true
+vim.opt.ww = "h,l"
