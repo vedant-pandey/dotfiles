0bc1fe86a39b49321c71d8fa231ca939f9757603
Vedant Pandey <vedant@Vedants-MacBook-Pro.local>
2023-07-25 07:25:37 +0530

    minimal install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# install stow
+# install neovim
+# install tmux
+# install rustup
+# install zap
+zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+# install lsd
+# install nerd fonts
+# install fnm && node 
+# install lazygit
+# install lunar vim
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+# install tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# install fd
+cargo install fd-find
+# install rip grep
+cargo install ripgrep
