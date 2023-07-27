c6f7d704b4ec53e085e4947a9808540678f60192
Vedant Pandey <vedantpandey46@gmail.com>
2023-07-27 22:26:04 +0530

    add zero lsp plugin

--- config/nvim/lua/vedant/plugins.lua
+++ config/nvim/lua/vedant/plugins.lua
@@ -76,6 +76,26 @@ require("lazy").setup({
     {
         "neovim/nvim-lspconfig",
     },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+                -- LSP Support
+                {'neovim/nvim-lspconfig'},             -- Required
+                {                                      -- Optional
+                'williamboman/mason.nvim',
+                build = function()
+                    pcall(vim.api.nvim_command, 'MasonUpdate')
+                end,
+            },
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        }
+    },
 })
 
 -- plugin configs
@@ -110,3 +130,16 @@ require("mason-lspconfig").setup_handlers {
         require("rust-tools").setup {}
     end
 }
+
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()
