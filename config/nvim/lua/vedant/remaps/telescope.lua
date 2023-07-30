9c57b5bda34f86e1754ff83cc17a586de260e997
Vedant Pandey <vedantpandey46@gmail.com>
2023-07-30 09:59:52 +0530

    change remaps

--- config/nvim/lua/vedant/remaps/telescope.lua
+++ config/nvim/lua/vedant/remaps/telescope.lua
@@ -1,12 +1,12 @@
 local telescope_builtin = require("telescope.builtin")
 
 vim.keymap.set('n', '<leader>F', telescope_builtin.find_files, {})
-vim.keymap.set('n', '<leader>ff', telescope_builtin.git_files, {})
-vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, {})
+vim.keymap.set('n', '<leader>f', telescope_builtin.git_files, {})
+-- vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, {})
 vim.keymap.set('n', '<leader>st', telescope_builtin.live_grep, {})
 vim.keymap.set('n', '<leader>bf', telescope_builtin.buffers, {})
-vim.keymap.set('n', '<leader>fm', telescope_builtin.marks, {})
-vim.keymap.set("n", "<leader>fv", "<cmd>vsplit<CR><cmd>Telescope find_files<cr>") -- split buffer and select what to open
+vim.keymap.set('n', '<leader>mf', telescope_builtin.marks, {})
+vim.keymap.set("n", "<leader>vf", "<cmd>vsplit<CR><cmd>Telescope find_files<cr>") -- split buffer and select what to open
 
 -- file browser remaps
-vim.keymap.set('n', '<leader>fe', "<cmd>Telescope file_browser<CR>", {})
+-- vim.keymap.set('n', '<leader>fe', "<cmd>Telescope file_browser<CR>", {})
