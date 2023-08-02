df3e02147b518cd767e3627ea46470c81781412b
Vedant Pandey <vedantpandey46@gmail.com>
2023-08-02 10:31:24 +0530

    update remaps

--- config/nvim/lua/vedant/remaps/telescope.lua
+++ config/nvim/lua/vedant/remaps/telescope.lua
@@ -1,7 +1,7 @@
 local telescope_builtin = require('telescope.builtin')
 
-vim.keymap.set({'n', 'v'}, '<leader>F', telescope_builtin.find_files, {})
-vim.keymap.set({'n', 'v'}, '<leader>f', telescope_builtin.git_files, {})
+vim.keymap.set({'n', 'v'}, '<leader>f', telescope_builtin.find_files, {})
+vim.keymap.set({'n', 'v'}, '<leader>F', telescope_builtin.git_files, {})
 -- vim.keymap.set({'n', 'v'}, '<leader>fh', telescope_builtin.help_tags, {})
 vim.keymap.set({'n', 'v'}, '<leader>st', telescope_builtin.live_grep, {})
 vim.keymap.set({'n', 'v'}, '<leader>bf', telescope_builtin.buffers, {})
