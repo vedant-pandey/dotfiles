e6f8bfca12831a9199d043f13848798d2ac7f42f
Vedant Pandey <vedantpandey46@gmail.com>
2023-08-25 12:44:40 +0530

    improve create links

--- create-links.sh
+++ create-links.sh
@@ -15,30 +15,41 @@ fi
 #     exit 1
 # fi
 
-ALL=false
+LINK_ALL=false
+LINK_CONFIG=false
+LINK_SSH=false
+LINK_HOME=false
 
 if echo $* | grep -e "-a" -q; then
-    ALL=true
+    LINK_ALL=true
+elif echo $* | grep -e "-c" -q; then
+    LINK_CONFIG=true
+elif echo $* | grep -e "-h" -q; then
+    LINK_HOME=true
+elif echo $* | grep -e "-s" -q; then
+    LINK_SSH=true
 else
-    echo "invalid flags"
+    echo "Invalid flag(s)"
 fi
 
-echo "$ALL"
+if $LINK_ALL || $LINK_CONFIG; then
+    echo -e "INIT LINKING CONFIGS\n"
+    stow -nRvt ~/.config config
+    echo -e "\nDONE LINKING CONFIGS\n"
+    echo -e "~~~~~~~~~~~~~~~~~~~~~~~\n"
+fi
 
-exit 1
+if $LINK_ALL || $LINK_HOME; then
+    echo -e "INIT LINKING HOME FILES\n"
+    stow -nRvt ~ home
+    echo -e "\nDONE LINKING HOME FILES\n"
+    echo -e "~~~~~~~~~~~~~~~~~~~~~~~\n"
+fi
 
-echo -e "INIT LINKING CONFIGS\n"
-stow -nRvt ~/.config config
-echo -e "\nDONE LINKING CONFIGS\n"
-echo -e "~~~~~~~~~~~~~~~~~~~~~~~\n"
-
-echo -e "INIT LINKING HOME FILES\n"
-stow -nRvt ~ home
-echo -e "\nDONE LINKING HOME FILES\n"
-echo -e "~~~~~~~~~~~~~~~~~~~~~~~\n"
-
-echo -e "INIT LINKING SSH KEYS\n"
-cd decrypted
-stow -nRvt ~/.ssh ssh
-echo -e "\nDONE LINKING SSH KEYS\n"
-echo -e "~~~~~~~~~~~~~~~~~~~~~~~\n"
+if $LINK_ALL || $LINK_SSH; then
+    echo -e "INIT LINKING SSH KEYS\n"
+    cd decrypted
+    stow -nRvt ~/.ssh ssh
+    echo -e "\nDONE LINKING SSH KEYS\n"
+    echo -e "~~~~~~~~~~~~~~~~~~~~~~~\n"
+fi
