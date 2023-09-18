413d5e02ab7c80ae266adf64bbfaee353c8fb373
Vedant Pandey <vedantpandey46@gmail.com>
2023-09-18 12:01:56 +0530

    minor changes

--- config/nvim/config.lua
+++ config/nvim/config.lua
@@ -84,7 +84,6 @@ lvim.plugins = {
             'DBUIFindBuffer',
         },
         init = function()
-            -- Your DBUI configuration
             vim.g.db_ui_use_nerd_fonts = 1
         end,
     },
