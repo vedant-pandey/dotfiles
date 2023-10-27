c3b67e6bbf200dab687f90f515911ff03d15a58b
Vedant Pandey <vedantpandey46@gmail.com>
2023-10-27 07:20:34 +0530

    add elixir lsp

--- config/nvim/config.lua
+++ config/nvim/config.lua
@@ -87,6 +87,35 @@ lvim.plugins = {
             vim.g.db_ui_use_nerd_fonts = 1
         end,
     },
+    {
+        "elixir-tools/elixir-tools.nvim",
+        version = "*",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            local elixir = require("elixir")
+            local elixirls = require("elixir.elixirls")
+
+            elixir.setup {
+                nextls = {enable = true},
+                credo = {},
+                elixirls = {
+                    enable = true,
+                    settings = elixirls.settings {
+                        dialyzerEnabled = false,
+                        enableTestLenses = false,
+                    },
+                    on_attach = function(client, bufnr)
+                        vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
+                        vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
+                        vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+                    end,
+                }
+            }
+        end,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+    },
 }
 
 -- basic stuff
@@ -176,6 +205,11 @@ lvim.keys.normal_mode["<leader>gp"] = ":lua require('goto-preview').goto_preview
 lvim.keys.normal_mode["<leader>ui"] = ":DBUIToggle<cr>"
 
 require('todo-comments').setup()
+require("elixir").setup({
+  nextls = {enable = false},
+  credo = {enable = true},
+  elixirls = {enable = true},
+})
 
 vim.opt.so = 16
 vim.opt.nu = true
