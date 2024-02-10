b8303d06c9b9d3590dd14b124cff97b346664e7e
Vedant Pandey <vedantpandey46@gmail.com>
2024-02-10 17:24:37 +0530

    nit:refactor

--- install-nix.sh
+++ install-nix.sh
@@ -14,13 +14,15 @@ nix run home-manager/master -- init --switch
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # Clone repo
 git clone https://github.com/vedant-pandey/dotfiles
-cd ./dotfiles/home-manager
-cp -r ./home-manager/.zshrc ~/.config/home-manager/
-cp -r ./home-manager/.tmux.conf ~/.config/home-manager/
-cp -r ./home-manager/home.nix ~/.config/home-manager/
-cp -r ./home-manager/flake.lock ~/.config/home-manager/
+cp -r ./dotfiles/home-manager/.zshrc ~/.config/home-manager/
+cp -r ./dotfiles/home-manager/.tmux.conf ~/.config/home-manager/
+cp -r ./dotfiles/home-manager/home.nix ~/.config/home-manager/
+cp -r ./dotfiles/home-manager/flake.lock ~/.config/home-manager/
+rm -rf ./dotfiles
 # Init user config
-nix eval --raw -f <(curl -L https://raw.githubusercontent.com/vedant-pandey/dotfiles/main/init-config.nix) > ~/.config/home-manager/user.toml
+curl -L https://raw.githubusercontent.com/vedant-pandey/dotfiles/main/init-config.nix > init-config.nix
+nix eval --raw -f init-config.nix > ~/.config/home-manager/user.toml
+rm init-config.nix
 # Enable home-manager
 home-manager switch
 # Install Zap
