8de2cda9dbbe57f310b3880e1d27105fdc126a89
Vedant Pandey <vedantpandey46@gmail.com>
2024-02-10 13:22:42 +0530

    Completely automated nix config

--- install-nix.sh
+++ install-nix.sh
@@ -17,5 +17,7 @@ cp -r ./home-manager/.zshrc ~/.config/home-manager/
 cp -r ./home-manager/.tmux.conf ~/.config/home-manager/
 cp -r ./home-manager/home.nix ~/.config/home-manager/
 cp -r ./home-manager/flake.lock ~/.config/home-manager/
+# Init user config
+nix eval --raw -f <(curl -L https://raw.githubusercontent.com/vedant-pandey/dotfiles/main/init-config.nix) > ~/.config/home-manager/user.toml
 # Enable home-manager
 home-manager switch
