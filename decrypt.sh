27606cc75b12652a8e261ca7cfb15ea5a3aecda1
Vedant Pandey <vedantpandey46@gmail.com>
2024-12-19 12:57:59 +0530

    feat: Complete single command setup

--- decrypt.sh
+++
@@ -1,5 +0,0 @@
-trap 'echo "Interrupt found exiting"' 1 2 3 6 15
-mkdir test
-cp -r ssh test
-cd test/ssh
-ansible-vault decrypt *
--- install-nix.sh
+++ install-nix.sh
@@ -1,31 +1,66 @@
 #!/bin/bash
 
-# Install Nix
+# Setup base directories
+echo "SETTING UP BASE DIRECTORIES"
+if [[ -d "~/personal" ]]; then
+    echo "Personal directory exists"
+else
+    echo "Creating personal directory"
+    mkdir ~/personal
+fi
+
+if [[ -d "~/work" ]]; then
+    echo "Work directory exists"
+else
+    echo "Creating work directory"
+    mkdir ~/work
+fi
+cd ~/personal
+
+echo "INSTALLING NIX"
 curl -L https://nixos.org/nix/install > nix.sh
 bash nix.sh --daemon --yes
 rm ./nix.sh
-# Enable Flakes
+echo "NIX INSTALLED"
+
+echo "ENABLING NIX FLAKES"
 echo "experimental-features = nix-command flakes" >> /etc/nix/nix.conf
-# Restart shell
-source /etc/profile
-# Download Home-manager
+
+source /etc/profile # reload shell
+
+echo "INSTALLING HOME MANAGER"
 nix run home-manager/master -- init --switch
 # Install TPM
+echo "INSTALLING TMUX PLUGIN MANAGER"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # Clone repo
+echo "CLONING DOTFILES"
 git clone https://github.com/vedant-pandey/dotfiles
-cp -r ./dotfiles/home-manager/.zshrc ~/.config/home-manager/
-cp -r ./dotfiles/home-manager/.tmux.conf ~/.config/home-manager/
-cp -r ./dotfiles/home-manager/home.nix ~/.config/home-manager/
-cp -r ./dotfiles/home-manager/flake.lock ~/.config/home-manager/
-rm -rf ./dotfiles
-# Init user config
-curl -L https://raw.githubusercontent.com/vedant-pandey/dotfiles/main/init-config.nix > init-config.nix
-nix eval --raw -f init-config.nix > ~/.config/home-manager/user.toml
-rm init-config.nix
+# Build user config
+
+echo "BUILDING USER CONFIG FILE"
+USER=${USER:-$(read -p "\$USER not set, Enter username: " envvalue && echo $envvalue)}
+HOME=${HOME:-$(read -p "\$HOME not set, Enter home dir: " envvalue && echo $envvalue)}
+GITHUB_USERNAME=${GITHUB_USERNAME:-$(read -p "\$GITHUB_USERNAME not set, Enter github username: " envvalue && echo $envvalue)}
+WORK_EMAIL=${WORK_EMAIL:-$(read -p "\$WORK_EMAIL not set, Enter work email: " envvalue && echo $envvalue)}
+PERSONAL_EMAIL=${PERSONAL_EMAIL:-$(read -p "\$PERSONAL_EMAIL not set, Enter personal email: " envvalue && echo $envvalue)}
+FULL_NAME=${FULL_NAME:-$(read -p "\$FULL_NAME not set, Enter full name: " envvalue && echo $envvalue)}
+
+cat > ./home-manager/user.toml << EOF
+user = "$USER"
+home = "$HOME"
+githubUsername = "$GITHUB_USERNAME"
+email = "$WORK_EMAIL"
+personalEmail = "$PERSONAL_EMAIL"
+fullName = "$FULL_NAME"
+system = "aarch64-darwin"
+EOF
+
+echo "USER CONFIG FILE CREATED"
 # Enable home-manager
+echo "ENABLING NIX HOME MANAGER"
 home-manager switch
-# Install Zap
+echo "INSTALLING ZAP ZSH"
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 --keep
 # Reload shell
-exec $SHELL
+echo "All installation done reload your environment by running \`exec \$SHELL\`"
